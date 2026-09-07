117d7c7a43bd6286551de2c50b75e3ab631a345e
Vastayalover <cuteerin2002@gmail.com>
2026-09-07 15:34:57 -0300

    Ejercicio 3
    Subida del ejercicio 3 en donde tuvimos que reemplazar el boto3 por URLlib por nuevos cambios en la politica AWS

--- Exercises/Exercise-3/main.py
+++ Exercises/Exercise-3/main.py
@@ -1,9 +1,30 @@
-import boto3
+import gzip
+import io 
+import urllib.request
 
 
 def main():
-    # tu código va aquí
-    pass
+    # Bajar el archivo wet.paths.gz
+    BASE = "https://data.commoncrawl.org/"
+    req = urllib.request.Request("https://data.commoncrawl.org/crawl-data/CC-MAIN-2022-05/wet.paths.gz")
+    with urllib.request.urlopen(req) as response:
+        raw = response.read()
+    # Descomprimir el archivo
+    buf = io.BytesIO(raw)
+    with gzip.open(buf, "rt", encoding="utf-8") as f:
+        primera = f.readline().strip() #string
+    # Bajar el segundo archivo
+    with urllib.request.urlopen(BASE + primera) as response:
+        with gzip.GzipFile(fileobj=response) as f:
+            for line in f:
+                print(line.decode("utf-8"), end="")
+
+
+    
+   
+
+
+    
 
 
 if __name__ == "__main__":
